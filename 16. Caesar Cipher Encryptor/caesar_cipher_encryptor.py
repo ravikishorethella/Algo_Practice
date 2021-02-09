bea0c8693dcd4426b2de148de6e4d748c92d6bbe
Ravi Thella <15951682+ravikishorethella@users.noreply.github.com>
2021-02-09 07:03:15 -0500

    Add files via upload

---
+++ 16. Caesar Cipher Encryptor/caesar_cipher_encryptor.py
@@ -0,0 +1,30 @@
+# time - O(n)
+# space - O(n)
+
+def caesarCipherEncryptor(string, key):
+    newLetters = []
+    newKey = key % 26
+    for letter in string:
+        newLetters.append(getNewLetter(letter, newKey))
+    return "".join(newLetters)
+
+def getNewLetter(letter, key):
+    newLetterCode = ord(letter) + key
+    return chr(newLetterCode) if newLetterCode <= 122 else chr(96+newLetterCode%122)
+
+
+# approach 2
+# time - O(n)
+# space - O(n)
+
+def caesarCipherEncryptor(string, key):
+    newLetters = []
+    newKey = key % 26
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    for letter in string:
+        newLetters.append(getNewLetter(letter, newKey, alphabet))
+    return "".join(newLetters)
+
+def getNewLetter(letter, key, alphabet):
+    newLetterCode = alphabet.index(letter) + key
+    return alphabet[newLetterCode] if newLetterCode <= 25 else alphabet[-1+newLetterCode % 25]
