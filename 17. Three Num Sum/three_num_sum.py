c5cf921b50794da8d366234b3ec2fcdec973c3d2
Ravi Thella <15951682+ravikishorethella@users.noreply.github.com>
2021-02-10 06:15:06 -0600

    code for three sum

--- 17. Three Num Sum/three_num_sum.py
+++ 17. Three Num Sum/three_num_sum.py
@@ -1,1 +1,24 @@
 
+'''
+Given an array, find the tripelts which sums up to the target
+'''
+# time - O(n^2)
+# space - O(n) - at most, we will have to store all the values from array into triplets
+def threeNumSum(array,target):
+    # using a two pointer technique so, we need to sort the array
+    array.sort()
+    triplets = []
+    for i in range(len(array)-2):
+        left = i+1
+        right = len(array)-1
+        while left < right:
+            currentSum = array[i] + array[left] + array[right]
+            if currentSum == target:
+                triplets.append([array[i], array[left], array[right]])
+                left = left + 1
+                right = right - 1
+            elif currentSum < target:
+                left = left + 1
+            elif currentSum > target:
+                right = right - 1
+    return triplets
