4f467daf7899580b69dce1af6137ead859a0723b
Ravi Thella <15951682+ravikishorethella@users.noreply.github.com>
2021-02-10 06:32:31 -0600

    fix the logic

--- 17. Three Num Sum/three_num_sum.py
+++ 17. Three Num Sum/three_num_sum.py
@@ -1,4 +1,3 @@
-
 '''
 Given an array, find the tripelts which sums up to the target
 '''
@@ -8,17 +7,22 @@ def threeNumSum(array,target):
     # using a two pointer technique so, we need to sort the array
     array.sort()
     triplets = []
-    for i in range(len(array)-2):
+    for i in range(len(array)):
         left = i+1
         right = len(array)-1
-        while left < right:
-            currentSum = array[i] + array[left] + array[right]
-            if currentSum == target:
-                triplets.append([array[i], array[left], array[right]])
-                left = left + 1
-                right = right - 1
-            elif currentSum < target:
-                left = left + 1
-            elif currentSum > target:
-                right = right - 1
+        if i == 0 or array[i-1] != array[i]:
+            while left < right:
+                currentSum = array[i] + array[left] + array[right]
+                if currentSum == target:
+                    triplets.append([array[i], array[left], array[right]])
+                    left = left + 1
+                    right = right - 1
+                    while left < right and array[left] == array[left - 1]:
+                        left = left + 1
+                elif currentSum < target:
+                    left = left + 1
+                elif currentSum > target:
+                    right = right - 1
     return triplets
+
+print(threeNumSum([-1,0,1,2,-1,-4], 0))
