08ae893513e8410b2ef2596fa0fae294c8623ac9
Ravi Thella <15951682+ravikishorethella@users.noreply.github.com>
2021-02-17 06:01:05 -0600

    Update bst_traversal.py

--- 25. BST Traversal/bst_traversal.py
+++ 25. BST Traversal/bst_traversal.py
@@ -1,1 +1,23 @@
 
+# time - O(n) | space - O(d) depth of the tree
+
+def inOrderTraversal(tree, array):
+    if tree is not None:
+        inOrderTraversal(tree.left, array)
+        array.append(tree.value)
+        inOrderTraversal(tree.right, array)
+    return array
+
+def preOrderTraversal(tree, array):
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraversal(tree.left, array)
+        preOrderTraversal(tree.right, array)
+    return array
+
+def postOrderTraversal(tree, array):
+    if tree is not None:
+        postOrderTraversal(tree.left, array)
+        postOrderTraversal(tree.right, array)
+        array.append(tree.value)
+    return array
